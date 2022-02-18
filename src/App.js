fd01097a2589496f50e7c205eb07372b65df4402
Gabriel Sartori <gsgabriel27@hotmail.com>
2022-02-18 19:27:14 -0300

    Requisito 3: Create component Title

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
+import Title from './components/Title';
 
 class App extends React.Component {
   render() {
+    const text = 'Texto';
     return (
-      <body>
+      <div>
         <Header />
         <SolarSystem />
-      </body>
+        <Title headline={ text } />
+      </div>
     );
   }
 }
