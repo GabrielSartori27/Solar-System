71461d70989d806247084b35458148bf03071c84
Gabriel Sartori <gsgabriel27@hotmail.com>
2022-02-19 20:44:01 -0300

    Requisito 5: Create component PlanetCard

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import PlanetCard from './components/PlanetCard';
 import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
@@ -8,6 +9,7 @@ class App extends React.Component {
       <div>
         <Header />
         <SolarSystem />
+        <PlanetCard planetName="Terra" planetImage="https://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2014/11/21/nasaplanetaterra01.jpg" />
       </div>
     );
   }
