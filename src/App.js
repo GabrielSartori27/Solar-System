36197b952ff111922ffc70323a2cb34feac463a5
Gabriel Sartori <gsgabriel27@hotmail.com>
2022-02-19 19:47:44 -0300

    Requisito 4: Add the Title component inside of the SolarSystem component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
-import Title from './components/Title';
 
 class App extends React.Component {
   render() {
-    const text = 'Texto';
     return (
       <div>
         <Header />
         <SolarSystem />
-        <Title headline={ text } />
       </div>
     );
   }
