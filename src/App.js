fb9ecad9ddc7c0ba9eff562a92844b44556709ff
Gabriel Sartori <gsgabriel27@hotmail.com>
2022-02-19 21:50:20 -0300

    Requisito 6: Render a list of planets isnide SolarSystem

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from './components/Header';
-import PlanetCard from './components/PlanetCard';
 import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
@@ -9,7 +8,6 @@ class App extends React.Component {
       <div>
         <Header />
         <SolarSystem />
-        <PlanetCard planetName="Terra" planetImage="https://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2014/11/21/nasaplanetaterra01.jpg" />
       </div>
     );
   }
